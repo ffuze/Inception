137e70979885817bbee8bf88401fa79e5ab3417d
alek <alekeggsdee92@gmail.com>
2026-05-30 18:33:54 +0200

    completed (hopefully) mariadb configuration

--- src/requirements/mariadb/tools/init_db.sh
+++ src/requirements/mariadb/tools/init_db.sh
@@ -5,18 +5,18 @@ set -e
 echo "MariaDB is starting to initialize..."
 
 # check if data directory of mysql exists, if not create it
-if [! -d "/var/lib/mysql/mysql"]; then
+if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "MySQL data directory does not exist! Creating..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
 fi
 
 # in order to have our db we need to configure the stuff for it,
 # since when maraiadb is installed in the first place, it will
-# basically be empty, so to fix this we create a temporary db
+# basically be empty, so to fix this we create a temporary db;
 # --skip-networking is very useful since it disconnects the container
 # from the network, so nobody can access it while we configure it
 echo "Starting temporary server for MariaDB..."
-mysqld --skip-networking --socket=/run/mysqld/mysqld.sock
+mysqld --skip-networking --socket=/run/mysqld/mysqld.sock \
         --user=mysql & pid="$!"
 
 # we wait for the temporary server to be functional by pinging
@@ -46,3 +46,6 @@ wait "$pid" || true
 
 echo "Initialization complete, starting MariaDB..."
 exec mysqld --user=mysql --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock
+
+# to check if the socket file is present: sudo docker exec -it <container_id_or_name> ls -l /run/mysqld/mysqld.sock
+# to check if the necessary mariadb files are present: docker exec -it <container_id_or_name> mysqladmin ping -u root -p
